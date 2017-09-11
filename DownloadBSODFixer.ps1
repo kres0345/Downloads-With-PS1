67f1212322be6d7c337db866b3e2ae01d30ed81b
Kresten Laust Faaborg Sckerl <kressckerl@gmail.com>
2017-09-11 17:46:12 +0200

    Added folder

--- DownloadBSODFixer.ps1
+++ DownloadBSODFixer.ps1
@@ -1,5 +1,6 @@
 #The Download Script
+new-item "c:\Users\$env:UserName\Desktop\BSOD fixer by Kresten" -itemtype directory
 $uri = "http://download1322.mediafire.com/0bdcu8haf2rg/d76ppqzsc3spw8c/BSOD+fixer+by+Kresten.exe"
-$out = "c:\Users\$env:UserName\Desktop\BSODFixer by Kresten.exe"
+$out = "c:\Users\$env:UserName\Desktop\BSOD fixer by Kresten\BSODFixer by Kresten.exe"
 Invoke-WebRequest -Uri $uri -OutFile $out
 & $out
--- DownloadDataExtractor.ps1
+++ DownloadDataExtractor.ps1
@@ -1,5 +1,6 @@
 #The Download Script
+new-item c:\Users\$env:UserName\Desktop\DataExtractor -itemtype directory
 $uri = "http://download1076.mediafire.com/y73t7w9u12ag/6j82a346gisordq/Data+extrator.exe"
-$out = "c:\Users\$env:UserName\Desktop\DataExtractor.exe"
+$out = "c:\Users\$env:UserName\Desktop\DataExtractor\DataExtractor.exe"
 Invoke-WebRequest -Uri $uri -OutFile $out
 & $out
