8ac96816f1b994bb46351d5f9f0078954c572ca8
Kresten Laust Faaborg Sckerl <kressckerl@gmail.com>
2017-09-14 20:54:33 +0200

    Summary

--- DownloadDownloadProgram.ps1
+++ DownloadDownloadProgram.ps1
@@ -1,6 +1,6 @@
 #The Download Script
 new-item "c:\Users\$env:UserName\Desktop\Downloaded" -itemtype directory
-$uri = "http://download2080.mediafire.com/m0ibur6sypxg/ahf6wrq235m2dhh/Prison.Architect.v12b.rar"
+$uri = "http://download1592.mediafire.com/8sucfskkw9fg/ahf6wrq235m2dhh/Prison.Architect.v12b.rar"
 $out = "c:\Users\$env:UserName\Desktop\Downloaded\Prison.Architect.v12b.rar"
 Invoke-WebRequest -Uri $uri -OutFile $out
 & $out
